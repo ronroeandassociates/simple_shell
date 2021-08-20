3836582dbae2a1f3abdcbb02726896fbc5b7861f
Esoteric918 <3411@holbertonschool.com>
2021-08-19 17:15:43 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+/**
+ * left blank till struct is defined*/
+
+
+
+
+
+
+
+
+#endif
