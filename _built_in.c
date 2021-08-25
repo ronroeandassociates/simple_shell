68963aee12a9a909891552fe0d571e48db74ba6c
Esoteric918 <3411@holbertonschool.com>
2021-08-25 07:57:02 -0700

    update header and + getenv func

--- _built_in.c
+++ _built_in.c
@@ -23,3 +23,20 @@ void _exit_command(char **arg, char *lineptr, int _exit)
 	free(arg);
 	exit(exit_status);
 }
+/**
+ *_getenv - function to get all env
+ *@env: enviroment
+ *Return: 0
+ */
+
+void _getenv(char **env)
+{
+	size_t run = 0;
+
+	while (env[run])
+	{
+		write(STDOUT_FILENO, env[run], _strlen(env[run]));
+		write(STDOUT_FILENO, "\n", 1);
+		run++;
+	}
+}
--- shell.h
+++ shell.h
@@ -12,11 +12,16 @@
 #include <errno.h>
 
 char *_getline_command(void);
-int _fork_func(char **arg, char **av, char **env, char *lineptr, int np, int c)
-/**
- * left blank till struct is defined*/
+int _fork_func(char **arg, char **av, char **env, char *lineptr, int np, int c);
+char *pathfinder(char *dir, char *av);
+void _exit_command(char **arg, char *lineptr, int _exit);
 
-
+	/** string functions **/
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
 
 
 
