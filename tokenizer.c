600028710d2fa224db0015b73e39f0228ebf39c7
Esoteric918 <85411745+Esoteric918@users.noreply.github.com>
2021-08-26 10:27:07 -0500

    func that sperates string into tokens

--- tokenizer.c
+++ tokenizer.c
@@ -1,12 +1,29 @@
 #include "shell.h"
 /**
- *
- *
- *
- *
+ * tokenizer - sperates string of words to tokens
+ * @buff: buffer
+ * @delim: character
+ * Return: token
  *
  */
 char **tokenizer(char *buff,conts char *delim)
 {
-	
+	char **token, *clone = NULL;
+    size_t words = 0;
+
+    token = malloc(sizeof(char*) * TOKEN_SIZE);
+
+    if (!token)
+    {
+        perror(STDERR_FILENO, "unable to allocat buffer");
+    }
+    clone = strtok(buff, delim);
+    for (; clone != NULL; clone = strtok(NULL, delim))
+    {
+        token[words] = strtok(clone);
+        words++;
+    }
+    token[words] = clone;
+    free(clone);
+    return (token);
 }
