6b1889213f3208dfe964239ecf7635f7f7d0e12a
Esoteric918 <85411745+Esoteric918@users.noreply.github.com>
2021-08-26 10:28:05 -0500

    shell

--- simple_shell.c
+++ simple_shell.c
@@ -11,3 +11,37 @@ int main(int ac, char **av, char **env)
 	char *getcommand = NULL, **user_command = NULL;
 	int pathValue = 0, _exit = 0, n = 0;
 	(void)ac;
+
+	while (1)
+	{
+		getcommand = _getline_command();
+		if (getcommand)
+		{
+			pathValue++;
+			user_command = *tokenizer(getcommand)
+			if (!user_command)
+			{
+				free(getcommand);
+				continue;
+			}
+			if (!_strcmp(user_command[0], "exit")) && user_command[1] == NULL)
+				_exit_command(user_command, getcommand _exit);
+			if (!_strcmp(user_command[0], "env"))
+				getenv(env);
+			else
+			{
+				n = _value_path(&user_command[0], env);
+				_exit = _fork_func(user_command, av, env, getcommand, pathValue, n)
+				if (n == 0)
+					free(user_command[0]);
+			}
+			free(user_command);
+		}
+		else
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			exit(_exit);
+		}
+		free(getcommand);
+	}
