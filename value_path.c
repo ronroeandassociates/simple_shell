857c76081d39882406a77801bfe6aa38c50ddc0b
ronroeandassociates <3321@holbertonschool.com>
2021-08-26 13:50:51 -0700

    cleaned up path for args to create value

--- value_path.c
+++ value_path.c
@@ -17,5 +17,28 @@ int _value_path(char **arg, char **env)
 	if (!path_rela)
 		return (-1);
 	token = _strtok(path_rela, ":");
+command = _strlen(*arg);
+	while (token)
+	{
+		value_path = _strlen(token);
+		path_absol = malloc(sizeof(char) * (value_path + command + 2));
+		if (!path_absol)
+		{
+			free(path_rela);
+			return (-1);
+		}
+		path_absol = _strcpy(path_absol, token);
+		_strcat(path_absol, "/");
+		_strcat(path_absol, *arg);
+		if (stat(path_absol, &stat_lineptr) == 0)
+		{
+			*arg = path_absol;
+			free(path_rela);
+			return (0);
+		}
+		free(path_absol);
+		token = _strtok(NULL, ":");
+	}
+	free(path_rela);
 	return (-1);
 }
