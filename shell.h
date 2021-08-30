fd198b4faa1cef5b90c7c9f3794e21b4e4eeeb75
Esoteric918 <3411@holbertonschool.com>
2021-08-30 09:35:19 -0700

    update header

--- shell.h
+++ shell.h
@@ -11,21 +11,23 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+extern char **environ;
 
 /** command line functions**/
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
 char *_getline_command(void);
-int _frk_func(char **arg, char **av, char **env, char *lineptr, int np, int c);
-char *pathfinder(char **env);
-void _exit_command(char **arg, char *lineptr, int _exit);
 void _getenv(char **env);
+char **_get_token(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
+char *lineptr, int np, int c);
 /** string functions **/
+int _strcmp(char *s1, char *s2);
 size_t _strncmp(char *s1, char *s2, size_t n);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
-int _value_path(char **arg, char **env)
-char *_strdup(char *str);
-char *lineptr(int np, int c);
-char *_strtok(char *str, const char *delim);
+int _putchar(char c);
+
 #endif
