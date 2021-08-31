b09e3e57761306fc0ec9827b6ce3d51aabf766af
Esoteric918 <3411@holbertonschool.com>
2021-08-31 08:38:59 -0700

    fixed env function

--- simple_shell.c
+++ simple_shell.c
@@ -27,7 +27,7 @@ int main(int ac, char **av, char **env)
 			if (!_strcmp(user_command[0], "exit") && user_command[1] == NULL)
 				_exit_command(user_command, getcommand, _exit);
 			if (!_strcmp(user_command[0], "env"))
-				getenv(*env);
+				_getenv(env);
 			else
 			{
 				n = _value_path(&user_command[0], env);
